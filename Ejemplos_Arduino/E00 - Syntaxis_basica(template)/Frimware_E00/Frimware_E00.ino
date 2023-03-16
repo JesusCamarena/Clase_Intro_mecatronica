48789e796b167619e51993ed6b14b6e40ba5fc0e
Jesus Camarena <jesus.garzacmn@uanl.edu.mx>
2023-03-15 18:14:44 -0600

    Primer commit

---
+++ Ejemplos_Arduino/E00 - Syntaxis_basica(template)/Frimware_E00/Frimware_E00.ino
@@ -0,0 +1,29 @@
+//" ¡No copies y pegues el código, analízalo e inspirate en el! "
+
+/*************************************************************************
+ *   Copyright (C) 2023 Jesús Camarena                                   *
+ *   jesus.garzacmn@uanl.edu.mx                                          *
+ *   Licencia: Attribution-NonCommercial-ShareAlike 3.0(CC-BY-NC-SA 3.0) *
+ *   https://creativecommons.org/licenses/by-nc-sa/3.0/                  *
+ *                                                                       * 
+ *   Ejemplo:                                                            *
+ *   Dispositivo: Arduino UNO                                            *
+ *   Lenguaje: Arduino                                                   *
+ *   Rev: 1.0                                                            *
+ *                                                     Fecha: 15/03/23   *
+ ************************************************************************/
+
+//comentario corto
+/*comentario largo */
+
+/*Se ejecuta solamente una vez*/
+void setup() 
+{
+    // Código de configuración inicial
+}//Fin setup
+
+/*Se ejecuta infinitamente*/
+void loop() 
+{
+    // Código que se ejecuta repetidamente
+}//Fin loop
---
+++ Ejemplos_Arduino/E01 - Escritura_pin_digital/README.md
@@ -0,0 +1,40 @@
+# :closed_book: Clase_Introduccion a la Mecatrónica
+
+## :large_blue_diamond: Descripción
+
+
+## :globe_with_meridians: Página web
+
+
+## :large_orange_diamond: Inicio
+
+### :electric_plug: Materiales
+* Tarjeta de desarrollo Arduino Uno Empaquetado DIP
+* Tarjeta de desarrollo Arduino Nano Empaquetado TQFP
+
+### :black_circle: Software IDE
+* Arduino IDE
+
+### :large_blue_circle: Software EDA
+* Proteus Design Suite
+
+### :red_circle: Software Modelado Electrónico
+* Fritzing
+* TinkerCad con Tarjeta de desarrollo Arduino Uno
+* https://www.tinkercad.com/things/6VEPsdMs4qr
+
+## :interrobang: Ayuda
+
+## :busts_in_silhouette: Autores
+* [Jesus Camarena](https://www.notion.so/didyde/Profesor-universitario-Dise-ador-de-hardware-para-sistemas-embebidos-81703493db3c44c4a75b49b2d536ea19)
+
+## :ballot_box_with_check: Historial de versiones
+* 1.0
+    * Initial Release
+
+## Licencia
+
+This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
+
+## :speech_balloon: Agradecimientos
+
